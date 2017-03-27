1916fe68c71e68fb78c707097ff7778a36cf8d56
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-27 16:59:44 -0300

    3.8-forward-e-redirect

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package br.com.LeonardoPSouzaPortfolio.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.LeonardoPSouzaPortfolio.brewer.model.Cerveja;
 /**
@@ -29,17 +31,22 @@ public class CervejasController {
 	/**
 	 * @RequestMapping - Mapeia o URL numa requisição POST
 	 * @Valid - Valida um objeto
-	 * BindingResult result - Mante os dados
+	 * BindingResult result - Mante os resultados
+	 * Model - Para forward
+	 * RedirectAttributes - Para Redirect
 	 * @return - Retorna a pagina HTML
 	 */
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
-			System.out.println(">>>> tem erro sim!");
+			model.addAttribute("mensagem", "Erro no formulário");
+			return "cerveja/CadastroCerveja";
 		}
 		
+		// Salvar no banco de dados...
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		System.out.println(">>> sku: " + cerveja.getSku());
-		return "redirect:/cervejas/novo"; 
+		return "redirect:/cervejas/novo";
 	}
 	
 }
