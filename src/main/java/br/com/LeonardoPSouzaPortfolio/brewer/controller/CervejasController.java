364221a61fec71d38f9d101507cc5a765db78899
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-27 16:11:42 -0300

    3.6-comecando-o-cadastro-de-cerveja

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
@@ -2,12 +2,35 @@ package br.com.LeonardoPSouzaPortfolio.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import br.com.LeonardoPSouzaPortfolio.brewer.model.Cerveja;
+/**
+ * 
+ * @author leonardops
+ *
+ * @Controller - Aponta que a classe é um controller
+ */
 @Controller
 public class CervejasController {
 
+	/**
+	 * @RequestMapping - Mapeia o URL numa requisição GET
+	 * @return - Retorna a pagina HTML
+	 */
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
 		return "cerveja/CadastroCerveja";
 	}
+	
+	/**
+	 * @RequestMapping - Mapeia o URL numa requisição POST
+	 * @return - Retorna a pagina HTML
+	 */
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(Cerveja cerveja) {
+		System.out.println(">>> sku: " + cerveja.getSku());
+		return "cerveja/CadastroCerveja";
+	}
+	
 }
