5a860f2904b8bc68ebdd9eeeff8349da3eafc13c
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-27 20:19:45 -0300

    5.4-prototipando-o-cadastro-da-cerveja

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
@@ -48,9 +48,4 @@ public class CervejasController {
 		return "redirect:/cervejas/novo";
 	}
 	
-	@RequestMapping("/cervejas/cadastro")
-	public String cadastro() {
-		return "cerveja/cadastro-produto";
-	}
-	
 }
