32443607c20b0486905e56e2b236770a828b04b4
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-27 20:35:36 -0300

    5.5-melhorando-a-organizacao-do-layout

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/config/WebConfig.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/config/WebConfig.java
@@ -18,6 +18,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import br.com.LeonardoPSouzaPortfolio.brewer.controller.CervejasController;
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 
 /**
  * 
@@ -61,6 +62,8 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		SpringTemplateEngine engine = new SpringTemplateEngine();
 		engine.setEnableSpringELCompiler(true);
 		engine.setTemplateResolver(templateResolver());
+		
+		engine.addDialect(new LayoutDialect());
 		return engine;
 	}
 
