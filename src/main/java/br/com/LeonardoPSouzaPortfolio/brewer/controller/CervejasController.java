f293d26b1aae1159d17449f76291dd4756531f5b
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-28 18:29:45 -0300

    6.3. Configurando níveis de log com Log4j2

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
@@ -2,6 +2,8 @@ package br.com.LeonardoPSouzaPortfolio.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
