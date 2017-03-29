1003c874d4d79dd13413df25649bf7a9c2294e56
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-29 10:27:13 -0300

    12.2. Modularizando a máscara de dinheiro

--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,22 @@
-$(function() {
-	var decimal = $('.js-decimal');
-	decimal.maskMoney({ decimal: ',', thousands: '.' });
+var Brewer = Brewer || {};
+
+Brewer.MaskMoney = (function() {
 	
-	var plain = $('.js-plain');
-	plain.maskMoney({ precision: 0, thousands: '.' });
+	function MaskMoney() {
+		this.decimal = $('.js-decimal');
+		this.plain = $('.js-plain');
+	}
+	
+	MaskMoney.prototype.enable = function() {
+		this.decimal.maskMoney({ decimal: ',', thousands: '.' });
+		this.plain.maskMoney({ precision: 0, thousands: '.' });
+	}
+	
+	return MaskMoney;
+	
+}());
+
+$(function() {
+	var maskMoney = new Brewer.MaskMoney();
+	maskMoney.enable();
 });
