7106afbc5e24438d03cda91aef293fc6bcebcc5f
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-29 17:11:19 -0300

    15.2. Prototipando a pesquisa da cerveja

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/CervejasController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.LeonardoPSouzaPortfolio.brewer.model.Cerveja;
 import br.com.LeonardoPSouzaPortfolio.brewer.model.Origem;
 import br.com.LeonardoPSouzaPortfolio.brewer.model.Sabor;
+import br.com.LeonardoPSouzaPortfolio.brewer.repository.Cervejas;
 import br.com.LeonardoPSouzaPortfolio.brewer.repository.Estilos;
 import br.com.LeonardoPSouzaPortfolio.brewer.service.CadastroCervejaService;
 /**
@@ -21,8 +23,10 @@ import br.com.LeonardoPSouzaPortfolio.brewer.service.CadastroCervejaService;
  * @author leonardops
  *
  * @Controller - Aponta que a classe é um controller
+ * @RequestMapping - Mapeia a classe
  */
 @Controller
+@RequestMapping("/cervejas")
 public class CervejasController {
 	
 	@Autowired
@@ -30,12 +34,15 @@ public class CervejasController {
 	
 	@Autowired
 	private CadastroCervejaService cadastroCervejaService;
+	
+	@Autowired
+	private Cervejas cervejas;
 
 	/**
 	 * @RequestMapping - Mapeia o URL numa requisição GET
 	 * @return - Retorna a pagina HTML
 	 */
-	@RequestMapping("/cervejas/novo")
+	@RequestMapping("/novo")
 	public ModelAndView novo(Cerveja cerveja) {
 		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
 		mv.addObject("sabores", Sabor.values());
@@ -52,7 +59,7 @@ public class CervejasController {
 	 * RedirectAttributes - Para Redirect
 	 * @return - Retorna a pagina HTML
 	 */
-	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(cerveja);
@@ -61,6 +68,17 @@ public class CervejasController {
 		cadastroCervejaService.salvar(cerveja);
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
 		return new ModelAndView("redirect:/cervejas/novo");
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("cerveja/PesquisaCervejas");
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("origens", Origem.values());
+		
+		mv.addObject("cervejas", cervejas.findAll());
+		return mv;
 	}
 	
 }
