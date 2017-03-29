244983bebcb2cc9a27a8bb64b48d040c7b97719e
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-29 10:12:10 -0300

    11.4. Tratando exceções com ExceptionHandler

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/EstilosController.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/EstilosController.java
@@ -56,12 +56,7 @@ public class EstilosController {
 			return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
 		}
 		
-		try {
-			estilo = cadastroEstiloService.salvar(estilo);
-		} catch (NomeEstiloJaCadastradoException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-		
+		estilo = cadastroEstiloService.salvar(estilo);
 		return ResponseEntity.ok(estilo);
 	}
 	
