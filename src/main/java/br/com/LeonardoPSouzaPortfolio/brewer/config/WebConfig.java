0870447dc2e60783d9cfea3d4dc4c1afb4fbdc64
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-28 23:46:14 -0300

    10.3. Convertendo valores numéricos

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/config/WebConfig.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/config/WebConfig.java
@@ -1,17 +1,23 @@
 package br.com.LeonardoPSouzaPortfolio.brewer.config;
 
+import java.math.BigDecimal;
+import java.util.Locale;
+
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
@@ -99,7 +105,21 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
 		conversionService.addConverter(new EstiloConverter());
 		
+		NumberStyleFormatter bigDecimalFormatter = new NumberStyleFormatter("#,##0.00");
+		conversionService.addFormatterForFieldType(BigDecimal.class, bigDecimalFormatter);
+		
+		NumberStyleFormatter integerFormatter = new NumberStyleFormatter("#,##0");
+		conversionService.addFormatterForFieldType(Integer.class, integerFormatter);
+		
 		return conversionService;
+	}
+	/**
+	 * 
+	 * @return localização do Brasil
+	 */
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 
 } 
--- src/main/resources/static/javascripts/brewer.js
+++ src/main/resources/static/javascripts/brewer.js
@@ -1,7 +1,7 @@
 $(function() {
 	var decimal = $('.js-decimal');
-	decimal.maskMoney();
+	decimal.maskMoney({ decimal: ',', thousands: '.' });
 	
 	var plain = $('.js-plain');
-	plain.maskMoney({ precision: 0 });
+	plain.maskMoney({ precision: 0, thousands: '.' });
 });
