beeb2091a4f9dcbd7d9ea63316dcda3527e32819
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-29 10:19:05 -0300

    11.5. Callbacks JPA - Salvando SKU com caixa alta

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/model/Cerveja.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/model/Cerveja.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
@@ -72,6 +74,11 @@ public class Cerveja {
 	@ManyToOne
 	@JoinColumn(name = "codigo_estilo")
 	private Estilo estilo;
+	
+	@PrePersist @PreUpdate
+	private void prePersistUpdate() {
+		sku = sku.toUpperCase();
+	}
 
 	public String getSku() {
 		return sku;
