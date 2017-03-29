fd5760fdf71a9306ad0214644f3d9a758b7f9a2a
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-29 12:17:54 -0300

    13.3. Novo elemento para mensagens

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/thymeleaf/BrewerDialect.java
@@ -8,6 +8,7 @@ import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
 import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.MessageElementTagProcessor;
 
 public class BrewerDialect extends AbstractProcessorDialect {
 
@@ -19,6 +20,7 @@ public class BrewerDialect extends AbstractProcessorDialect {
 	public Set<IProcessor> getProcessors(String dialectPrefix) {
 		final Set<IProcessor> processadores = new HashSet<>();
 		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
+		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
