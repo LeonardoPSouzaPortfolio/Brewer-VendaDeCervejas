c6f715aa5878aa0fc4548738eb90ba58461c9557
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-30 04:56:26 -0300

    15.14. Ordenando a pesquisa por SKU ou nome

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/page/PageWrapper.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/page/PageWrapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -44,6 +46,40 @@ public class PageWrapper<T> {
 	
 	public String urlParaPagina(int pagina) {
 		return uriBuilder.replaceQueryParam("page", pagina).build(true).encode().toUriString();
+	}
+	
+	public String urlOrdenada(String propriedade) {
+		UriComponentsBuilder uriBuilderOrder = UriComponentsBuilder
+				.fromUriString(uriBuilder.build(true).encode().toUriString());
+		
+		String valorSort = String.format("%s,%s", propriedade, inverterDirecao(propriedade));
+		
+		return uriBuilderOrder.replaceQueryParam("sort", valorSort).build(true).encode().toUriString();
+	}
+	
+	public String inverterDirecao(String propriedade) {
+		String direcao = "asc";
+		
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null;
+		if (order != null) {
+			direcao = Sort.Direction.ASC.equals(order.getDirection()) ? "desc" : "asc";
+		}
+		
+		return direcao;
+	}
+	
+	public boolean descendente(String propriedade) {
+		return inverterDirecao(propriedade).equals("asc");
+	}
+	
+	public boolean ordenada(String propriedade) {
+		Order order = page.getSort() != null ? page.getSort().getOrderFor(propriedade) : null; 
+		
+		if (order == null) {
+			return false;
+		}
+		
+		return page.getSort().getOrderFor(propriedade) != null ? true : false;
 	}
 	
 }
