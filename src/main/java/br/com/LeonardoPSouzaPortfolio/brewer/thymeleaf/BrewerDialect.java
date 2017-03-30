6eb1fc8c3ac36f2ee4cb90987a1cae4d1a7ea40c
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-30 17:01:10 -0300

    15.15. Componente Thymeleaf para ordenação

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/thymeleaf/BrewerDialect.java
@@ -9,6 +9,7 @@ import org.thymeleaf.standard.StandardDialect;
 
 import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
 import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.MessageElementTagProcessor;
+import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.OrderElementTagProcessor;
 
 public class BrewerDialect extends AbstractProcessorDialect {
 
@@ -21,6 +22,7 @@ public class BrewerDialect extends AbstractProcessorDialect {
 		final Set<IProcessor> processadores = new HashSet<>();
 		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
 		processadores.add(new MessageElementTagProcessor(dialectPrefix));
+		processadores.add(new OrderElementTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
