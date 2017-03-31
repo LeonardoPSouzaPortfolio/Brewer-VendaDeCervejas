601abb7345625e373d94cb906c4f7e4c22092228
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-03-31 01:01:13 -0300

    16.1. Configurando máscara de telefone com jQuery

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/page/PageWrapper.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/page/PageWrapper.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Order;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
 public class PageWrapper<T> {
@@ -17,7 +16,11 @@ public class PageWrapper<T> {
 
 	public PageWrapper(Page<T> page, HttpServletRequest httpServletRequest) {
 		this.page = page;
-		this.uriBuilder = ServletUriComponentsBuilder.fromRequest(httpServletRequest);
+		//this.uriBuilder = ServletUriComponentsBuilder.fromRequest(httpServletRequest); // Problemas com espaço na pesquisa
+		String httpUrl = httpServletRequest.getRequestURL().append(
+				httpServletRequest.getQueryString() != null ? "?" + httpServletRequest.getQueryString() : "")
+				.toString().replaceAll("\\+", "%20");
+		this.uriBuilder = UriComponentsBuilder.fromHttpUrl(httpUrl);
 	}
 	
 	public List<T> getConteudo() {
