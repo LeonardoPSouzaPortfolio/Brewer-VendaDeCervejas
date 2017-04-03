6639cc1430a69f252008ba06992f75d660830a3b
Leonardo P. de Souza <leonardo.p.desouza1994@gmail.com>
2017-04-03 16:50:22 -0300

    16.6. Aplicando migração estado e cidade

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/ClientesController.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/controller/ClientesController.java
@@ -1,19 +1,25 @@
 package br.com.LeonardoPSouzaPortfolio.brewer.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.LeonardoPSouzaPortfolio.brewer.model.TipoPessoa;
+import br.com.LeonardoPSouzaPortfolio.brewer.repository.Estados;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClientesController {
 
+	@Autowired
+	private Estados estados;
+	
 	@RequestMapping("novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
 		mv.addObject("tiposPessoa", TipoPessoa.values());
+		mv.addObject("estados", estados.findAll());
 		return mv;
 	}
 	
---
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/repository/Estados.java
@@ -0,0 +1,9 @@
+package br.com.LeonardoPSouzaPortfolio.brewer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.LeonardoPSouzaPortfolio.brewer.model.Estado;
+
+public interface Estados extends JpaRepository<Estado, Long> {
+
+}
