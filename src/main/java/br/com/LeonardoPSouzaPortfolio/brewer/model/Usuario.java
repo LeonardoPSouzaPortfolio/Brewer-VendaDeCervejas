0e99f586acb3a70ec551d30ec408d814e40ec3b4
leonardopdesouza <leonardopdesouza@leonardopdesouza-Aspire-E5-573G>
2017-04-18 11:46:22 -0300

    18.4. Validador customizado para confirmação de senha

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/model/Usuario.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/model/Usuario.java
@@ -13,11 +13,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
+import br.com.LeonardoPSouzaPortfolio.brewer.validation.AtributoConfirmacao;
+
+@AtributoConfirmacao(atributo = "senha", atributoConfirmacao = "confirmacaoSenha", message = "Confirmação da senha não confere")
 @Entity
 @Table(name = "usuario")
 public class Usuario implements Serializable {
@@ -36,6 +40,9 @@ public class Usuario implements Serializable {
 	private String email;
 
 	private String senha;
+	
+	@Transient
+	private String confirmacaoSenha;
 
 	private Boolean ativo;
 
@@ -102,6 +109,14 @@ public class Usuario implements Serializable {
 
 	public void setGrupos(List<Grupo> grupos) {
 		this.grupos = grupos;
+	}
+
+	public String getConfirmacaoSenha() {
+		return confirmacaoSenha;
+	}
+
+	public void setConfirmacaoSenha(String confirmacaoSenha) {
+		this.confirmacaoSenha = confirmacaoSenha;
 	}
 
 	@Override
