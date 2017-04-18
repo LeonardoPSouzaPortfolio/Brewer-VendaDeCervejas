d9ae8a9e7f64c8f3c0da3c14570b37a15db61fbd
leonardopdesouza <leonardopdesouza@leonardopdesouza-Aspire-E5-573G>
2017-04-18 11:24:17 -0300

    18.3. Desafio - Começando o cadastro do usuário

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/model/Usuario.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/model/Usuario.java
@@ -45,7 +45,6 @@ public class Usuario implements Serializable {
 				, inverseJoinColumns = @JoinColumn(name = "codigo_grupo"))	
 	private List<Grupo> grupos;
 
-	@NotNull(message = "Data de nascimento é obrigatório")
 	@Column(name = "data_nascimento")
 	private LocalDate dataNascimento;
 
