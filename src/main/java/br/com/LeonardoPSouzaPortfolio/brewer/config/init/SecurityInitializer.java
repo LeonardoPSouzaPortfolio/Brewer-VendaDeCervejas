26ebcec9b879de8efd9a1e40a9291918712af424
leonardopdesouza <leonardopdesouza@leonardopdesouza-Aspire-E5-573G>
2017-04-20 15:34:45 -0300

    19.12. Duração da sessão

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/config/init/SecurityInitializer.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/config/init/SecurityInitializer.java
@@ -1,15 +1,21 @@
 package br.com.LeonardoPSouzaPortfolio.brewer.config.init;
 
+import java.util.EnumSet;
+
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
+import javax.servlet.SessionTrackingMode;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
+	
 	@Override
 	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+		//servletContext.getSessionCookieConfig().setMaxAge(20); // tempo de vida da sessão em segundos
+		servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE)); // sessão por cookie 
+		
 		FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter",
 				new CharacterEncodingFilter());
 		characterEncodingFilter.setInitParameter("encoding", "UTF-8");
