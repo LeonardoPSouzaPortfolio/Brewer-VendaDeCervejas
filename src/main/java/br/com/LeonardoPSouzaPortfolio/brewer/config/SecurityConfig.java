4bd415711a61c463922a19eb6b32d295080316a4
leonardopdesouza <leonardopdesouza@leonardopdesouza-Aspire-E5-573G>
2017-04-20 10:27:10 -0300

    19.2. Configurando o Spring Security no projeto

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/config/SecurityConfig.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/config/SecurityConfig.java
@@ -1,16 +1,47 @@
 package br.com.LeonardoPSouzaPortfolio.brewer.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class SecurityConfig {
+/**
+ * @EnableWebSecurity - habilita webSecurity
+ *
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	/**
+	 * configura as autenticações e permissões
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.and()
+			.csrf().disable();
+	}
+	
+	/**
+	 * 
+	 * @return senha com BCrypt
+	 */
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 }
