aa9c9762b98bbb11a0d2e316171cc7f9e4166277
leonardopdesouza <leonardopdesouza@leonardopdesouza-Aspire-E5-573G>
2017-04-23 16:12:44 -0300

    21.1. Implementando menu lateral

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/thymeleaf/BrewerDialect.java
@@ -8,6 +8,7 @@ import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
 import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.MenuAttributeTagProcessor;
 import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.MessageElementTagProcessor;
 import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.OrderElementTagProcessor;
 import br.com.LeonardoPSouzaPortfolio.brewer.thymeleaf.processor.PaginationElementTagProcessor;
@@ -25,6 +26,7 @@ public class BrewerDialect extends AbstractProcessorDialect {
 		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		processadores.add(new OrderElementTagProcessor(dialectPrefix));
 		processadores.add(new PaginationElementTagProcessor(dialectPrefix));
+		processadores.add(new MenuAttributeTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
