e9c35f3815fe8c6ad1d3abcff2b95b11c388bd03
leonardopdesouza <leonardopdesouza@leonardopdesouza-Aspire-E5-573G>
2017-04-25 12:05:42 -0300

    21.5. Melhorando usabilidade da página de pesquisa de usuários

--- src/main/resources/static/javascripts/multiselecao.js
+++ src/main/resources/static/javascripts/multiselecao.js
@@ -5,15 +5,19 @@ Brewer.MultiSelecao = (function() {
 	function MultiSelecao() {
 		this.statusBtn = $('.js-status-btn');
 		this.selecaoCheckbox = $('.js-selecao');
+		this.selecaoTodosCheckbox = $('.js-selecao-todos');
 	}
 	
 	MultiSelecao.prototype.iniciar = function() {
 		this.statusBtn.on('click', onStatusBtnClicado.bind(this));
+		this.selecaoTodosCheckbox.on('click', onSelecaoTodosClicado.bind(this));
+		this.selecaoCheckbox.on('click', onSelecaoClicado.bind(this));
 	}
 	
 	function onStatusBtnClicado(event) {
 		var botaoClicado = $(event.currentTarget);
 		var status = botaoClicado.data('status');
+		var url = botaoClicado.data('url');
 		
 		var checkBoxSelecionados = this.selecaoCheckbox.filter(':checked');
 		var codigos = $.map(checkBoxSelecionados, function(c) {
@@ -22,7 +26,7 @@ Brewer.MultiSelecao = (function() {
 		
 		if (codigos.length > 0) {
 			$.ajax({
-				url: '/brewer/usuarios/status',
+				url: url,
 				method: 'PUT',
 				data: {
 					codigos: codigos,
@@ -34,6 +38,22 @@ Brewer.MultiSelecao = (function() {
 			});
 			
 		}
+	}
+	
+	function onSelecaoTodosClicado() {
+		var status = this.selecaoTodosCheckbox.prop('checked');
+		this.selecaoCheckbox.prop('checked', status);
+		statusBotaoAcao.call(this, status);
+	}
+	
+	function onSelecaoClicado() {
+		var selecaoCheckboxChecados = this.selecaoCheckbox.filter(':checked');
+		this.selecaoTodosCheckbox.prop('checked', selecaoCheckboxChecados.length >= this.selecaoCheckbox.length);
+		statusBotaoAcao.call(this, selecaoCheckboxChecados.length);
+	}
+	
+	function statusBotaoAcao(ativar) {
+		ativar ? this.statusBtn.removeClass('disabled') : this.statusBtn.addClass('disabled');
 	}
 	
 	return MultiSelecao;
