2185c31178a1cf3d60106e0664b666980cf067cb
leonardopdesouza <leonardopdesouza@leonardopdesouza-Aspire-E5-573G>
2017-04-26 00:04:19 -0300

    23.4. Testando a tabela de itens de venda

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/venda/TabelaItensVenda.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/venda/TabelaItensVenda.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.LeonardoPSouzaPortfolio.brewer.model.Cerveja;
 import br.com.LeonardoPSouzaPortfolio.brewer.model.ItemVenda;
 
 public class TabelaItensVenda {
@@ -15,6 +16,15 @@ public class TabelaItensVenda {
 				.map(ItemVenda::getValorTotal)
 				.reduce(BigDecimal::add)
 				.orElse(BigDecimal.ZERO);
+	}
+	
+	public void adicionarItem(Cerveja cerveja, Integer quantidade) {
+		ItemVenda itemVenda = new ItemVenda();
+		itemVenda.setCerveja(cerveja);
+		itemVenda.setQuantidade(quantidade);
+		itemVenda.setValorUnitario(cerveja.getValor());
+		
+		itens.add(itemVenda);
 	}
 	
 }
--- src/test/java/br/com/LeonardoPSouzaPortfolio/brewer/venda/TabelaItensVendaTest.java
+++ src/test/java/br/com/LeonardoPSouzaPortfolio/brewer/venda/TabelaItensVendaTest.java
@@ -1,11 +1,13 @@
 package br.com.LeonardoPSouzaPortfolio.brewer.venda;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import br.com.LeonardoPSouzaPortfolio.brewer.model.Cerveja;
 
 public class TabelaItensVendaTest {
 
@@ -19,6 +21,33 @@ public class TabelaItensVendaTest {
 	@Test
 	public void deveCalcularValorTotalSemItens() throws Exception {
 		assertEquals(BigDecimal.ZERO, tabelaItensVenda.getValorTotal());
+	}
+	
+	@Test
+	public void deveCalcularValorTotalComUmItem() throws Exception {
+		Cerveja cerveja = new Cerveja();
+		BigDecimal valor = new BigDecimal("8.90");
+		cerveja.setValor(valor);
+		
+		tabelaItensVenda.adicionarItem(cerveja, 1);
+		
+		assertEquals(valor, tabelaItensVenda.getValorTotal());
+	}
+	
+	@Test
+	public void deveCalcularValorTotalComVariosItens() throws Exception {
+		Cerveja c1 = new Cerveja();
+		BigDecimal v1 = new BigDecimal("8.90");
+		c1.setValor(v1);
+		
+		Cerveja c2 = new Cerveja();
+		BigDecimal v2 = new BigDecimal("4.99");
+		c2.setValor(v2);
+		
+		tabelaItensVenda.adicionarItem(c1, 1);
+		tabelaItensVenda.adicionarItem(c2, 2);
+		
+		assertEquals(new BigDecimal("18.88"), tabelaItensVenda.getValorTotal());
 	}
 	
 }
