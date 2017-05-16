e5a19bcf6561dabb0b7c1d5d6a82ccd70e8fd94e
leonardopdesouza <leonardopdesouza@leonardopdesouza-Aspire-E5-573G>
2017-05-16 16:12:32 -0300

    23.19. Ajustando menu lateral

--- src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/thymeleaf/processor/MenuAttributeTagProcessor.java
+++ src/main/java/br/com/LeonardoPSouzaPortfolio/brewer/thymeleaf/processor/MenuAttributeTagProcessor.java
@@ -35,7 +35,7 @@ public class MenuAttributeTagProcessor extends AbstractAttributeTagProcessor {
 		HttpServletRequest request = ((IWebContext) context).getRequest();
 		String uri = request.getRequestURI();
 		
-		if (uri.startsWith(menu)) {
+		if (uri.matches(menu)) {
 			String classesExistentes = tag.getAttributeValue("class");
 			structureHandler.setAttribute("class", classesExistentes + " is-active");
 		}
